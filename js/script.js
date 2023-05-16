3663ef3876bcbf80bce80be54bc8a8cc71f82862
Nitin Kumar Saxena <saxenanitin72@gmail.com>
2023-05-16 15:34:42 +0530

    svg progress complete

--- js/script.js
+++ js/script.js
@@ -1,2 +1,33 @@
-// gsap.registerPlugin(drawSVG,)
-gsap.to('#Opaque_Ring',{drawSVG:"0%"  ,duration:4})
+// gsap.to('#Opaque_Ring',{strokeDasharray:"1000"  ,duration:4})
+
+
+//svg progress animation during scroll
+const tl = gsap.timeline({
+  scrollTrigger:{
+    trigger:'#page2',
+    start:"690% 730%",
+    end: "630% top",
+    markers:true,
+    scrub:1
+  }
+})
+
+tl.to("#Opaque_Ring",{strokeDasharray:"142.857",duration:2})
+.to("#Opaque_Ring",{strokeDasharray:"285.714",duration:2,delay:4})
+.to("#Opaque_Ring",{strokeDasharray:"416.571",duration:2,delay:6})
+.to("#Opaque_Ring",{strokeDasharray:"540.428",duration:2,delay:6.5})
+.to("#Opaque_Ring",{strokeDasharray:"682.571",duration:2,delay:6.7})
+.to("#Opaque_Ring",{strokeDasharray:"825.428",duration:2,delay:8})
+.to("#Opaque_Ring",{strokeDasharray:"1000",duration:2,delay:8.5})
+
+
+
+window.onscroll = function() {changeColor()};
+
+function changeColor() {
+  if (document.documentElement.scrollTop > 50 ) {
+    document.getElementById("#transring").classList += 'red';
+  } else {
+    // document.getElementById("myP").className = "";
+  }
+}
